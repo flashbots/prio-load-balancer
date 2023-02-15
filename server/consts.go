d57214e3c5b6056f67479f30070366a27d2eb623
Chris Hager <chris@linuxuser.at>
2023-02-15 11:06:02 +0100

    make PayloadMaxBytes 2MB

--- server/consts.go
+++ server/consts.go
@@ -11,7 +11,7 @@ var (
 	RequestMaxTries       = GetEnvInt("RETRIES_MAX", 3)              // 3 tries means it will be retried 2 additional times, and on third error would fail
 	MaxQueueItemsHighPrio = GetEnvInt("ITEMS_HIGHPRIO_MAX", 0)       // Max number of items in high-prio queue. 0 means no limit.
 	MaxQueueItemsLowPrio  = GetEnvInt("ITEMS_LOWPRIO_MAX", 0)        // Max number of items in low-prio queue. 0 means no limit.
-	PayloadMaxBytes       = GetEnvInt("PAYLOAD_MAX_KB", 1536) * 1024 // Max payload size in bytes. If a payload sent to the webserver is larger, it returns "400 Bad Request".
+	PayloadMaxBytes       = GetEnvInt("PAYLOAD_MAX_KB", 2048) * 1024 // Max payload size in bytes. If a payload sent to the webserver is larger, it returns "400 Bad Request".
 
 	RequestTimeout       = time.Duration(GetEnvInt("REQUEST_TIMEOUT", 5)) * time.Second       // Time between creation and receive in the node worker, after which a SimRequest will not be processed anymore
 	ServerJobSendTimeout = time.Duration(GetEnvInt("JOB_SEND_TIMEOUT", 2)) * time.Second      // How long the server tries to send a job into the nodepool for processing
